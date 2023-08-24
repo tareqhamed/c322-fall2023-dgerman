aba1222ecdae4c6cbca19607f887468fd9ee3788
c212 <40395648+c212@users.noreply.github.com>
2023-08-24 19:24:26 -0400

    Update Range.java with Unit Testing

--- homework/Range.java
+++ homework/Range.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class Range {
   char start, stop;
   public Range(char start, char stop) {
@@ -6,5 +8,17 @@ public class Range {
   }
   public char ross() {
     return (char) (start + (int) (Math.random() * (stop - start +  1)));
+  }
+  public static void main(String[] args) {
+     ArrayList<Range> robert = new ArrayList<Range>();
+     robert.add(new Range('0', '3'));
+     robert.add(new Range('5', '6'));
+     robert.add(new Range('8', '8'));
+     for (int i = 0; i < robert.size(); i++) {
+       for (int j = 0; j < 12; j++) {
+         System.out.print(robert.get(i).ross());
+       }
+       System.out.println();
+     }
   }
 }
