97a617b66ac3e31b6eb3eada17fce441c10c25f3
c212 <40395648+c212@users.noreply.github.com>
2023-08-24 17:54:29 -0400

    Update RandomStringGenerator.java

--- homework/RandomStringGenerator.java
+++ homework/RandomStringGenerator.java
@@ -1,13 +1,13 @@
 import java.util.ArrayList;
-
 public class RandomStringGenerator {
-  public ArrayList<Range> aidan = new ArrayList<Range>();
+  public ArrayList<Range> aidan = new ArrayList<Range>(); // outside of a constructor
   public static void main(String[] args) {
     RandomStringGenerator generator = new RandomStringGenerator();
     // System.out.println( generator );
     generator.addRange('a', 'z');
     generator.addRange('A', 'Z');
-    String s = generator.nextString(10);
+    generator.addRange('0', '9');
+    String s = generator.nextString(Integer.parseInt(args[0]));
     System.out.println( s );
   }
   public void addRange(char start, char stop) {
@@ -15,7 +15,12 @@ public class RandomStringGenerator {
     System.out.println( this.aidan );
   }
   public String nextString(int length) {
-
-    return "something";
+    String answer = "";
+    for (int i = 0; i < length; i++) {
+       int j = (int)(Math.random() * aidan.size());
+       //  System.out.println(j);
+       answer += (this.aidan.get(j).ross() + "");
+    }
+    return answer;
   }
 }
