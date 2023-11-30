eb0331e29d6e5ab7d21ca5761cd316b78a63d4e0
tareqhamed <107215589+tareqhamed@users.noreply.github.com>
2023-11-30 12:01:06 -0500

    Add files via upload

---
+++ homework/Homework 7/Adapter/Object Adapter/TeacherForDADA.java
@@ -0,0 +1,75 @@
+public class TeacherForDADA implements TeachingDADA {
+
+    public void defenseAgainstCruciatusCurse(){
+        System.out.println(
+                "This curse inflicts extreme pain. The overall "
+                        + "effect of the curse depends on the state of the "
+                        + "mind of the caster and how intent the caster is "
+                        + "on inflicting torture. "
+        );
+        System.out.println(
+                "No specific counter-curse is known for this curse. "
+                        + "One could try the shield charm Protego Horribilis. "
+                        + "An individual expecting to be hit with this curse "
+                        + "could also carry around a sneakoscope, which is a "
+                        + "miniature glass spinning top that emits shrill "
+                        + "sounds if when someone with evil intent is in the "
+                        + "vicinity. "
+        );
+    }
+
+    public void awarenessOfHorcrux(){
+        System.out.println(
+                "A horcrux can be a living being or an object. A dark "
+                        + "wizard wanting immortality stores a portion of "
+                        + "his/her soul in a horcrux. "
+        );
+        System.out.println( "Can be destroyed with basilisk's venom. ");
+    }
+
+    public void defenseAgainstPetrification(){
+        System.out.println(
+                "Petrification turns a living being into a stone-like "
+                        + "state of unconsciousness. "
+        );
+        System.out.println(
+                "Can be revived with mandrake restorative draught. "
+        );
+    }
+
+    public void defenseAgainstSectumsempra(){
+        System.out.println(
+                "Sectumsempra curse causes a victim's skin to be "
+                        + "slashed according to the motions of the wand held "
+                        + "by the caster.  However, if the caster does not "
+                        + "move the wand but only points it at a limb of the "
+                        + "victim, the victim may lose that limb. "
+        );
+        System.out.println(
+                "There exists a counter curse to Secturmsempra --- "
+                        + "it is called Vulnera Sanentur.  Snape used the "
+                        + "counter curse when Harry threw the Sectumsempra"
+                        + " curse at Draco Molfoy. "
+        );
+    }
+
+    public void defenseAgainstAvadaKedavra() {
+        System.out.println(
+                "This darkest of the dark arts is the killing curse. "
+        );
+        System.out.println(
+                "Is blocked by the power of love.  That is, if you are "
+                        + "`shielded' by the love given to you by people in your "
+                        + "past or at present, this darkest of the curses will"
+                        + "be deflected off you. Harry was shielded against this "
+                        + "curse because his mother made the ultimate sacrifice "
+                        + "of love for him. The only other being who is known to "
+                        + "possess a shield against this curse was Voldemort "
+                        + "because of his Horcruxes. This curse can also"
+                        + "be blocked if a victim hides behind a sold object. "
+                        + "The object will burst into flames when hit by the "
+                        + "curse.  The curse hits a victim in the form of a "
+                        + "green bolt of lightening. "
+        );
+    }
+}
