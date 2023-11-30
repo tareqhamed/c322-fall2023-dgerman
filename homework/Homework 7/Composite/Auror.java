7d31cf60ed990c9cde379788a4d08c06b5a444ab
tareqhamed <107215589+tareqhamed@users.noreply.github.com>
2023-11-30 12:51:50 -0500

    Add files via upload

--- homework/Homework 7/Composite/Auror.java
+++ homework/Homework 7/Composite/Auror.java
@@ -1,27 +1,27 @@
 import java.util.*;
 
 public class Auror extends Wizard {
-	
+
     private List<Wizard> deathEatersAssigned = new ArrayList<Wizard>();
-    private boolean hasPreviousExperienceInDealingWithDeathEaters 
-                                                               = false;
-	
+    private boolean hasPreviousExperienceInDealingWithDeathEaters
+            = false;
+
     public Auror( String firstname, String lastname ) {
         super( firstname, lastname );
     }
-	
-    public boolean isAurorInGoodStanding() 
-                    throws UnauthorizedAccessException {
+
+    public boolean isAurorInGoodStanding()
+            throws UnauthorizedAccessException {
         if (!this.getWizardryLevel().equals(
-                   WizardTraits.WizardryLevel.Master)) {
-            System.out.println( 
-              "This wizard is not qualified to capture a Death Eater");
+                WizardTraits.WizardryLevel.Master)) {
+            System.out.println(
+                    "This wizard is not qualified to capture a Death Eater");
             return false;
         }
-        if (!this.getWizardryStrength().equals( 
-                   WizardTraits.WizardryStrength.DADA ) ) {
-            System.out.println( 
-              "This wizard is not qualified to capture a Death Eater");
+        if (!this.getWizardryStrength().equals(
+                WizardTraits.WizardryStrength.DADA ) ) {
+            System.out.println(
+                    "This wizard is not qualified to capture a Death Eater");
             return false;
         }
         for (SchoolGraduatedFrom school : SchoolGraduatedFrom.values()){
@@ -29,29 +29,29 @@ public class Auror extends Wizard {
                 return true;
             }
         }
-        System.out.println( 
-              "This wizard is not qualified to capture a Death Eater");
+        System.out.println(
+                "This wizard is not qualified to capture a Death Eater");
         return false;
-    }	
+    }
 
     public boolean acceptOrderToCaptureDeathEater(Wizard deathEater)
-                                   throws UnauthorizedAccessException {
+            throws UnauthorizedAccessException {
         if ( !hasPreviousExperienceInDealingWithDeathEaters ) {
             if ( isAurorInGoodStanding() ) {
                 deathEatersAssigned.add( deathEater );
                 hasPreviousExperienceInDealingWithDeathEaters = true;
-                System.out.println( 
-                     "Order to capture " + deathEater + " accepted" );
+                System.out.println(
+                        "Order to capture " + deathEater + " accepted" );
                 return true;
             }
         } else if (deathEatersAssigned.size()  < 5) {
             deathEatersAssigned.add( deathEater );
-                System.out.println( 
-                   "Order to capture " + deathEater + " accepted" );
+            System.out.println(
+                    "Order to capture " + deathEater + " accepted" );
             return true;
         }
-        System.out.println( 
-             "Order to capture " + deathEater + " was not accepted" );
+        System.out.println(
+                "Order to capture " + deathEater + " was not accepted" );
         return false;
     }
 }
