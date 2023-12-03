ecf782058d60fd42cdcc96f88ac9b65eaa9c215f
tareqhamed <107215589+tareqhamed@users.noreply.github.com>
2023-12-03 15:05:17 -0500

    Add files via upload

--- homework/HW 9/Proxy/ClientClass.java
+++ homework/HW 9/Proxy/ClientClass.java
@@ -1,7 +1,7 @@
 public class ClientClass {
     public static void main( String[] args ) {
-        DarkLord youKnowWho = 
-                         DarkLord.makeInstanceOfDarkLord("Voldemort");
+        DarkLord youKnowWho =
+                DarkLord.makeInstanceOfDarkLord("Voldemort");
         DarkWizard proxy = new DarkWizard("Quirrell");
         proxy.setCanCastUnforgivableCurses(true);
         youKnowWho.goal("steal the Philosopher's Stone",  proxy );
